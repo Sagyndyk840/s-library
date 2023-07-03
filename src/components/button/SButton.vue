0d8aecf90ac233d33b6d435c6a9a15e7475bc1e0
sagingaliev <sagindik2003@gmail.com>
2023-07-03 07:59:07 +0500

    process

--- src/components/button/SButton.vue
+++ src/components/button/SButton.vue
@@ -1,9 +1,22 @@
-<script lang="ts" setup></script>
+<script setup lang="ts">
+import type {SkyButtonProps} from "types/components/button";
+
+const props = defineProps<SkyButtonProps>()
+</script>
 
 <template>
   <div>
-    <button>
-      <slot></slot>
+    <button :type="props.type" :class="[
+      props.color,
+      props.align,
+      props.size,
+      props.padding,
+      {
+        'outlined': props.outlined,
+        'rounded': props.rounded,
+      }
+    ]">
+      <slot :class="props.textColor">{{ props.label }}</slot>
     </button>
   </div>
 </template>
---
+++ types/components/button/index.ts
@@ -0,0 +1,15 @@
+export interface SkyButtonProps {
+    type?: string,
+    color?: string,
+    textColor?: string,
+    label?: string,
+    icon?: string,
+    iconRight?: string,
+    align?: string,
+    size?: string,
+    padding?: string,
+    rounded?: boolean,
+    outlined?: boolean,
+    loading?: boolean
+}
+
